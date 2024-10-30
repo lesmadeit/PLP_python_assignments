70502cc5cc588bab12aaeecf6c09dabae8ba533d
lesmadeit <lesmwendwa@gmail.com>
2024-10-30 16:49:01 +0300

    assignment complete

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('Enter Your Name: ')
+age = input('Enter Your Age: ')
+location = input('Enter Your Location: ')
+
+print('Hello', name, 'you are', age, 'years old and live in', location)
